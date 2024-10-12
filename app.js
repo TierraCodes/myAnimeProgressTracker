9580924b1165650092b379cc1929db994f13c46c
TierraCodes <112350897+TierraCodes@users.noreply.github.com>
2024-10-11 20:05:56 -0400

    forgotten commit

--- app.js
+++ app.js
@@ -1,100 +1,35 @@
 let indexPics = document.getElementsByClassName("indexImages");
 let backButton = document.getElementById("backButton");
 let nextButton = document.getElementById("nextButton");
+let currentIndex = 0;
 
-nextButton.addEventListener("click", ()=>{
-    console.log(indexPics[0].id);
-})
+nextButton.addEventListener("click", goNext)
 
-backButton.addEventListener("click", ()=>{
-    console.log(indexPics[0].id);
-})
+backButton.addEventListener("click", goBack)
 
 function goNext(){
-    if (indexPics[0].id==="pic1"){
-        indexPics[0].setAttribute("id", "pic4");
-        indexPics[1].setAttribute("id", "pic1");
-        indexPics[2].setAttribute("id", "pic2");
-        indexPics[3].setAttribute("id", "pic3");
-        console.log(indexPics[0].id)
-        return
-    } else if (indexPics[0].id==="pic2") {
-        indexPics[0].setAttribute("id", "pic1");
-        indexPics[1].setAttribute("id", "pic2");
-        indexPics[2].setAttribute("id", "pic3");
-        indexPics[3].setAttribute("id", "pic4");
-        console.log(indexPics[0].id)
-        return
-    } else if (indexPics[0].id==="pic3") {
-        indexPics[0].setAttribute("id", "pic2");
-        indexPics[1].setAttribute("id", "pic3");
-        indexPics[2].setAttribute("id", "pic4");
-        indexPics[3].setAttribute("id", "pic1");
-        console.log(indexPics[0].id)
-        return
-    } else if (indexPics[0].id==="pic4") {
-        indexPics[0].setAttribute("id", "pic3");
-        indexPics[1].setAttribute("id", "pic4");
-        indexPics[2].setAttribute("id", "pic1");
-        indexPics[3].setAttribute("id", "pic2");
-        console.log(indexPics[0].id)
-        return
-    } else {
-        return "help"
+    indexPics[currentIndex].setAttribute("id","pic5");
+    currentIndex++
+    if (currentIndex>=indexPics.length){
+        currentIndex = 0;
     }
+    indexPics[currentIndex].setAttribute("id","pic1");
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 function goBack(){
-    if (indexPics[0].id==="pic1"){
-        indexPics[0].setAttribute("id", "pic2");
-        indexPics[1].setAttribute("id", "pic3");
-        indexPics[2].setAttribute("id", "pic4");
-        indexPics[3].setAttribute("id", "pic1");
-        console.log(indexPics[0].id)
-        return
-    } else if (indexPics[0].id==="pic2") {
-        indexPics[0].setAttribute("id", "pic3");
-        indexPics[1].setAttribute("id", "pic4");
-        indexPics[2].setAttribute("id", "pic1");
-        indexPics[3].setAttribute("id", "pic2");
-        console.log(indexPics[0].id)
-        return
-    } else if (indexPics[0].id==="pic3") {
-        indexPics[0].setAttribute("id", "pic4");
-        indexPics[1].setAttribute("id", "pic1");
-        indexPics[2].setAttribute("id", "pic2");
-        indexPics[3].setAttribute("id", "pic3");
-        console.log(indexPics[0].id)
-        return
-    } else if (indexPics[0].id==="pic4") {
-        indexPics[0].setAttribute("id", "pic1");
-        indexPics[1].setAttribute("id", "pic2");
-        indexPics[2].setAttribute("id", "pic3");
-        indexPics[3].setAttribute("id", "pic4");
-        console.log(indexPics[0].id)
-        return
-    } else {
-        return "help"
+    indexPics[currentIndex].setAttribute("id", "pic2");
+    currentIndex--
+    if (currentIndex<0){
+        currentIndex = indexPics.length-1;
     }
+    indexPics[currentIndex].setAttribute("id","pic1");
+}
+
+let progressBar = document.getElementById("progressBar")
+
+progressBar.background.addEventListener("click", showProgress);
+
+function showProgress(){
+    let progress = 62.66
+    progressBar.style.background = `linear-gradient(green ${progress}%, floralwhite ${100-progress}%)`
 }
