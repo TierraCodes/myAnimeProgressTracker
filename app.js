dfe1f45ab6a33867d120e22508627c09c75013cc
TierraCodes <112350897+TierraCodes@users.noreply.github.com>
2024-10-13 21:43:11 -0400

    app fully functional!!

--- app.js
+++ app.js
@@ -1,11 +1,5 @@
 const indexPics = document.getElementsByClassName("indexImages");
-const backButton = document.getElementById("backButton");
-const nextButton = document.getElementById("nextButton");
 let currentIndex = 0;
-
-//nextButton.addEventListener("click", goNext)
-
-//backButton.addEventListener("click", goBack)
 
 function goNext(){
     indexPics[currentIndex].setAttribute("id","pic5");
@@ -27,24 +21,26 @@ function goBack(){
 }
 
 let emptyProgressBar = document.getElementsByClassName("emptyProgressBar")
-let fullProgressBar = document.getElementsByClassName("fullProgressBar")
 let pageName = document.getElementsByTagName("title")
 let progress = 0
-
-
 let i = 0
+
 emptyProgressBar[i].addEventListener("click", () =>{
     if (pageName[i].innerText == "My Hero Academia"){
         progress = 63
+    } else if(pageName[i].innerText == "Attack on Titan"){
+        progress = 100
+    } else if(pageName[i].innerText == "Horimiya"){
+        progress = 100
+    } else if(pageName[i].innerText == "From Me to You"){
+        progress = 98
+    } else if(pageName[i].innerText == "Saiki K"){
+        progress = 48
     } else {
-        progress = 25
+        progress = 0
     }
-
-    emptyProgressBar[i].classList.toggle("fullProgressBar")
-    emptyProgressBar[i].classList.remove("emptyProgressBar")
     
-    fullProgressBar[i].innerHTML = ''
-    fullProgressBar[i].style.background = `linear-gradient(to right, green ${progress}%, floralwhite 0%)`
-    fullProgressBar[i].style.transitionDuration = "3s"
-    i++
+    emptyProgressBar[i].innerHTML = `${progress}%`
+    emptyProgressBar[i].style.transitionDuration = "10s"
+    emptyProgressBar[i].style.background = `linear-gradient(to right, green ${progress}%, floralwhite 0%)`
 })
